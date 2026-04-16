d4e2a92037b3cd7b2265eb64984bbdf9bc7d8dce
Lutsu Andrian <lutsu.andrian@gmail.com>
2026-04-16 21:34:24 +0200

    Initial Unreal project setup and basic Portal actor
    Added build scripts, module files, and a basic APortal actor class. No gameplay logic yet; this establishes the foundation for further development.

---
+++ Source/LudumDare59/LudumDare59.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LudumDare59.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LudumDare59, "LudumDare59" );
